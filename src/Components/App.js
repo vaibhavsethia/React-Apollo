f7882882685740deccbe34d46adc95e05dfa3863
vaibhavsethia <vaibhavsethia1998@gmail.com>
2020-05-31 23:45:20 +0530

    Server Setup and Basic Links

---
+++ src/Components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../Styles/App.css';
+import LinkList from './LinkList'
+function App() {
+  return (
+    <div className="App">
+     <LinkList></LinkList>
+    </div>
+  );
+}
+
+export default App;
