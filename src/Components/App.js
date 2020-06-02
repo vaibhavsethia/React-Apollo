cb32faaf62408d9a1f4a52cce40df1a3cb92d5e7
vaibhavsethia <vaibhavsethia1998@gmail.com>
2020-06-02 20:59:03 +0530

    Completion

--- src/Components/App.js
+++ src/Components/App.js
@@ -4,21 +4,26 @@ import LinkList from './LinkList'
 import CreateLink from './CreateLinks';
 import Header from './Header'
 import { Switch, Route } from 'react-router-dom'
+import Login from './Login'
+import Search from './Search'
+
 
 class App extends Component {
   render() {
-    return (
-      <div className="center w85">
-        <Header />
-        <div className="ph3 pv1 background-gray">
-          <Switch>
-            <Route exact path="/" component={LinkList} />
-            <Route exact path="/create" component={CreateLink} />
-          </Switch>
-        </div>
+  return (
+    <div className='center w85'>
+      <Header />
+      <div className='ph3 pv1 background-gray'>
+        <Switch>
+          <Route exact path='/' component={LinkList} />
+          <Route exact path='/create' component={CreateLink} />
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/search' component={Search} />
+        </Switch>
       </div>
-    )
-  }
+    </div>
+  )
+}
   
 }
 
--- src/index.js
+++ src/index.js
@@ -4,7 +4,11 @@ import './Styles/index.css'
 import App from './Components/App'
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom'
-
+import { setContext } from 'apollo-link-context'
+import { AUTH_TOKEN } from './Constants'
+import { split } from 'apollo-link'
+import { WebSocketLink } from 'apollo-link-ws'
+import { getMainDefinition } from 'apollo-utilities'
 
 import { ApolloProvider } from 'react-apollo'
 import { ApolloClient } from 'apollo-client'
@@ -16,8 +20,39 @@ const httpLink = createHttpLink({
   uri: 'http://localhost:4000'
 })
 
+const authLink = setContext((_, {
+  headers
+}) => {
+  const token = localStorage.getItem(AUTH_TOKEN)
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  }
+})
+
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      authToken: localStorage.getItem(AUTH_TOKEN),
+    }
+  }
+})
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query)
+    return kind === 'OperationDefinition' && operation === 'subscription'
+  },
+  wsLink,
+  authLink.concat(httpLink)
+)
+
 const client = new ApolloClient({
-  link: httpLink,
+  link,
   cache: new InMemoryCache()
 })
 
